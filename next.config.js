18c20b7b6f09c9055f3ddf7185a672613b652fbf
strawhat-dev <trankn4@gmail.com>
2021-12-17 06:00:31 -0800

    update

--- next.config.js
+++ next.config.js
@@ -8,10 +8,12 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
+const userConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
-});
+  i18n: { locales: ['en'], defaultLocale: 'en' },
+};
+
+module.exports = (phase, defaultConfig) => {
+  const config = { ...defaultConfig, ...userConfig };
+  return withBundleAnalyzer(config);
+};
