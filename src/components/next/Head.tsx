75c7933a966700daf8fd35c9a3480459f8692846
strawhat-dev <trankn4@gmail.com>
2022-12-14 08:08:01 -0800

    adjust Head component interface

--- src/components/next/Head.tsx
+++ src/components/next/Head.tsx
@@ -15,7 +15,7 @@ export const Head = ({
       <meta charSet="utf-8" />
       <title>{title}</title>
       <meta name="description" content={description} />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="icon" href="/favicon.ico" />
     </NextHead>
     {children}
