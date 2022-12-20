755a42028ccb1f1c52cada943c884789becc5734
strawhat-dev <trankn4@gmail.com>
2022-12-19 17:16:39 -0800

    use export instead of declaration in global

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,8 +1,6 @@
 // https://fettblog.eu/typescript-react-generic-forward-refs/
-declare global {
-  declare module 'react' {
-    function forwardRef<T, P = Record<string, unknown>>(
-      render: (props: P, ref: React.Ref<T>) => React.ReactElement | null
-    ): (props: P & React.RefAttributes<T>) => React.ReactElement | null;
-  }
+export module 'react' {
+  function forwardRef<T, P = Record<string, unknown>>(
+    render: (props: P, ref: React.Ref<T>) => React.ReactElement | null
+  ): (props: P & React.RefAttributes<T>) => React.ReactElement | null;
 }
